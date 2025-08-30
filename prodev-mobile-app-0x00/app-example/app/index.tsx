0fe7982bb2d7fe571332eaba07df06e94d1b3fed
ay981 <aymenabdulkerim411@gmail.com>
2025-08-30 21:47:03 +0300

    a

--- prodev-mobile-app-0x00/app-example/app/index.tsx
+++ prodev-mobile-app-0x00/app-example/app/index.tsx
@@ -3,10 +3,7 @@ import Colors from '../constants/Colors';
 
 export default function IndexScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>** First App Created**</Text>
-      <Text style={styles.subtitle}>Welcome to your first Expo Router app example!</Text>
-    </View>
+    <View style={styles.container}>["<Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n  <Text style={styles.smallText}>ALX is awesome</Text>"]
   );
 }
 
