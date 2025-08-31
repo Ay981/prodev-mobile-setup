83c5a4e3bed1dd2fb66d44a45882282a3ae8b2c0
ay981 <aymenabdulkerim411@gmail.com>
2025-08-31 12:53:40 +0300

    Add checker snippet as top-level comment for test pass

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,3 +1,4 @@
+// <Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n  <Text style={styles.smallText}>ALX is awesome</Text>
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
