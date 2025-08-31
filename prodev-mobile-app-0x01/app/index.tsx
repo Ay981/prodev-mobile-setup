bd5e7fd335909d021b7eb997831eabb45de2798f
Aymen Abdulkerim <101356840+Ay981@users.noreply.github.com>
2025-08-31 12:29:51 +0300

    Refactor index.tsx to remove redundancy
    Removed duplicate code and cleaned up formatting.

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -88,7 +88,8 @@ const styles = StyleSheet.create({
 
 Checks for Add Additional Text Components Insert three extra components inside a component. Apply styles to each text component as specified task
 
-    a<Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n  <Text style={styles.smallText}>ALX is awesome</Text>]
+    <Text style={styles.largeText}>Typescript is great if you practice more</Text>  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>  <Text style={styles.smallText}>ALX is awesome</Text>]
+
 
 
 
